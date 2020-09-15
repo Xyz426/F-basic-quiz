9d7929d56376f507701651ca8e12d96c03ae12d7
xyz <xyzzhybz@163.com>
2020-09-15 21:54:14 +0800

    [xyz]fix eslint

--- src/app.js
+++ src/app.js
@@ -1,32 +1,33 @@
 import "./style/index.scss";
-import {requestUserInfo, requestUserEducationsInfo} from './request'
+import { requestUserInfo, requestUserEducationsInfo } from "./request";
 
 const url = window.location.href;
 const id = url.split("users/")[1];
 
-const getUser = async() => {
-    const data = await requestUserInfo(id);
-    if(data.age === undefined){
-        document.getElementById("photo").setAttribute("alt","连图片都没有");
-        document.getElementById("header-introduction").innerHTML =  `啥也没有`;
-    }else{
-        document.getElementById("photo").setAttribute("src",data.avatar);
-        document.getElementById("header-introduction").innerHTML =  `MY NAME IS ${data.name} ${data.age}YO AND THIS IS MY RESUME/CV`;
-        document.getElementById("about-me-description").innerHTML = data.description;
-    }
-}
+const getUser = async () => {
+  const data = await requestUserInfo(id);
+  if (data.age === undefined) {
+    document.getElementById("photo").setAttribute("alt", "连图片都没有");
+    document.getElementById("header-introduction").innerHTML = `啥也没有`;
+  } else {
+    document.getElementById("photo").setAttribute("src", data.avatar);
+    document.getElementById(
+      "header-introduction"
+    ).innerHTML = `MY NAME IS ${data.name} ${data.age}YO AND THIS IS MY RESUME/CV`;
+    document.getElementById("about-me-description").innerHTML =
+      data.description;
+  }
+};
 
-const getUserEducations = async() => {
-    const data = await requestUserEducationsInfo(id);    
-    const ul = document.getElementById("education-ul");
-    
-    for(let i=0;i<data.length;i++){
-        ul.innerHTML += `<li><span class="education-introduction-year">${data[i].year}</span> 
-                <p class="education-introduction-content"><b>${data[i].title}</b>${data[i].description}</p></li>`
-    }
-    
-    console.log(data);
-}
+const getUserEducations = async () => {
+  const data = await requestUserEducationsInfo(id);
+  const ul = document.getElementById("education-ul");
+
+  data.forEach((item) => {
+    ul.innerHTML += `<li><span class="education-introduction-year">${item.year}</span> 
+                <p class="education-introduction-content"><b>${item.title}</b>${item.description}</p></li>`;
+  });
+};
 
 getUser();
 getUserEducations();
--- src/request.js
+++ src/request.js
@@ -1,15 +1,15 @@
 const URL = "http://localhost:8080/users";
 
-const requestUserInfo = async(id) =>{
-    const response = await fetch(URL+`/${id}`,{method: "GET",});
-    const data = await response.json();
-    return data; 
-}
+const requestUserInfo = async (id) => {
+  const response = await fetch(`${URL}/${id}`, { method: "GET" });
+  const data = await response.json();
+  return data;
+};
 
-const requestUserEducationsInfo = async(id) =>{
-    const response = await fetch(URL+`/${id}/educations`,{method: "GET",});
-    const data = await response.json();
-    return data; 
-}
+const requestUserEducationsInfo = async (id) => {
+  const response = await fetch(`${URL}/${id}/educations`, { method: "GET" });
+  const data = await response.json();
+  return data;
+};
 
-export {requestUserInfo, requestUserEducationsInfo};
+export { requestUserInfo, requestUserEducationsInfo };
